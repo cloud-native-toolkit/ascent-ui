2e2c098516c5ec81791b3b8763f53299ec32667a
MuthuSundaravadivel <muthu.infoavc@gmail.com>
2021-03-01 22:28:20 +0530

    Delete Service Integration

--- src/services/services/service.api.ts
+++ src/services/services/service.api.ts
@@ -3,6 +3,7 @@ import { ServiceDataModel } from "../../models/services/serviceDataModel";
 export abstract class ServiceDataApi {
     abstract async getServices(): Promise<ServiceDataModel[]>;
     abstract async getServiceDetails(serviceId: string): Promise<ServiceDataModel>;
+    abstract async doDeleteService(serviceId: string): Promise<ServiceDataModel>;
 }
 
 
