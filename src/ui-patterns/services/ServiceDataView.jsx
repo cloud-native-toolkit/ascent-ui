8dc2d337b3fd749c787528f9533fab0ef906fdf3
MuthuSundaravadivel <muthu.infoavc@gmail.com>
2021-03-03 16:40:03 +0530

    Modal UI Complted

--- src/ui-patterns/services/ServiceDataView.jsx
+++ src/ui-patterns/services/ServiceDataView.jsx
@@ -26,8 +26,11 @@ class ServiceDataView extends Component {
         super(props);
         this.state = {
             data: [],
-            headerData: headerData
+            headerData: headerData,
+            show: false
         };
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
 
     }
     async componentDidMount() {
@@ -61,133 +64,132 @@ class ServiceDataView extends Component {
         });
 
     }
-    doOpenForm() {
+    showModal = () => {
+        this.setState({ show: true });
+    };
 
-    }
+    hideModal = () => {
+        this.setState({ show: false });
+    };
     render() {
         const data = this.state.data;
         const headers = this.state.headerData;
+        console.log(this.state.show);
         return (
-            <div className="bx--grid">
-                <div className="bx--row">
-                    <div className="bx--col-lg-16">
-                        <br></br>
-                        <h2 className="landing-page__subheading">
-                            Services
-                        </h2>
-                        <br></br>
-                        <p>
-                            List of IBM Cloud services
-                        </p>
-                        <br></br>
+            <><FormModal show={this.state.show} handleClose={this.hideModal} />
+                <div className="bx--grid">
+                    <div className="bx--row">
+                        <div className="bx--col-lg-16">
+                            <br></br>
+                            <h2 className="landing-page__subheading">
+                                Services
+                            </h2>
+                            <br></br>
+                            <p>
+                                List of IBM Cloud services
+                            </p>
+                            <br></br>
+                        </div>
                     </div>
-                </div>
 
-                <div className="bx--row">
+                    <div className="bx--row">
 
-                    <DataTable rows={data} headers={headers}>
-                        {({
-                            rows,
-                            headers,
-                            getHeaderProps,
-                            getSelectionProps,
-                            getToolbarProps,
-                            getBatchActionProps,
-                            getRowProps,
-                            getTableProps,
-                            selectedRows,
-                            onInputChange
+                        <DataTable rows={data} headers={headers}>
+                            {({
+                                rows,
+                                headers,
+                                getHeaderProps,
+                                getSelectionProps,
+                                getToolbarProps,
+                                getBatchActionProps,
+                                getRowProps,
+                                getTableProps,
+                                selectedRows,
+                                onInputChange
 
-                        }) => (
-                                <TableContainer>
-                                    <TableToolbar>
-                                        <TableBatchActions {...getBatchActionProps()}>
-                                            <TableBatchAction
-                                                tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
-                                                renderIcon={Delete}
-                                                onClick={() => this.batchActionClick(selectedRows)}>
-                                                Delete
-                                            </TableBatchAction>
-                                            {/*<TableBatchAction
-                                                tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
-                                                renderIcon={Save}
-                                                onClick={batchActionClick(selectedRows)}>
-                                                Save
-                                            </TableBatchAction>
-                                            <TableBatchAction
-                                                tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
-                                                renderIcon={Download}
-                                                onClick={batchActionClick(selectedRows)}>
-                                                Download
-                                            </TableBatchAction>*/}
-                                        </TableBatchActions>
-                                        <TableToolbarContent>
-                                            {/* pass in `onInputChange` change here to make filtering work */}
-                                            <TableToolbarSearch onChange={onInputChange} />
-                                            <TableToolbarMenu>
-                                                <TableToolbarAction >
-                                                    Action 1
-                                        </TableToolbarAction>
-                                                <TableToolbarAction>
-                                                    Action 2
-                                        </TableToolbarAction>
-                                                <TableToolbarAction >
-                                                    Action 3
-                                        </TableToolbarAction>
-                                            </TableToolbarMenu>
-                                            <Button onClick={() => this.doOpenForm()} >Add</Button>
-                                        </TableToolbarContent>
-                                    </TableToolbar>
-                                    <Table {...getTableProps()}>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableSelectAll {...getSelectionProps()} />
-                                                {headers.map((header) => (
-                                                    <TableHeader key={header.key} {...getHeaderProps({ header })}>
-                                                        {header.header}
-                                                    </TableHeader>
-                                                ))}
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {rows.map((row) => (
-                                                <TableRow key={row.id} {...getRowProps({ row })}>
-                                                    <TableSelectRow {...getSelectionProps({ row })} />
-                                                    {row.cells.map((cell) => (
-                                                        <TableCell key={cell.id}>{cell.value}</TableCell>
+                            }) => (
+                                    <TableContainer>
+                                        <TableToolbar>
+                                            <TableBatchActions {...getBatchActionProps()}>
+                                                <TableBatchAction
+                                                    tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+                                                    renderIcon={Delete}
+                                                    onClick={() => this.batchActionClick(selectedRows)}>
+                                                    Delete
+                                                </TableBatchAction>
+
+                                            </TableBatchActions>
+                                            <TableToolbarContent>
+
+                                                <TableToolbarSearch onChange={onInputChange} />
+                                                <TableToolbarMenu>
+                                                    <TableToolbarAction>
+                                                        Action 1
+                                                    </TableToolbarAction>
+                                                    <TableToolbarAction>
+                                                        Action 2
+                                                    </TableToolbarAction>
+                                                    <TableToolbarAction>
+                                                        Action 3
+                                                    </TableToolbarAction>
+                                                </TableToolbarMenu>
+                                                <Button
+                                                    tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}
+                                                    size="small"
+                                                    kind="primary"
+                                                    onClick={() => this.showModal()}>Add</Button>
+                                            </TableToolbarContent>
+                                        </TableToolbar>
+                                        <Table {...getTableProps()}>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableSelectAll {...getSelectionProps()} />
+                                                    {headers.map((header) => (
+                                                        <TableHeader key={header.key} {...getHeaderProps({ header })}>
+                                                            {header.header}
+                                                        </TableHeader>
                                                     ))}
-                                                    <TableCell className="bx--table-column-menu">
-                                                        <OverflowMenu light flipped >
-                                                            <OverflowMenuItem itemText="Edit" />
-                                                        </OverflowMenu>
-                                                    </TableCell>
+                                                    <TableHeader />
                                                 </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            )}
-                    </DataTable>
-                    <div style={{ width: '800px' }}>
-                        <Pagination
-                            backwardText="Previous page"
-                            forwardText="Next page"
-                            itemsPerPageText="Items per page:"
-                            page={1}
-                            pageNumberText="Page Number"
-                            pageSize={10}
-                            pageSizes={[
-                                10,
-                                20,
-                                30,
-                                40,
-                                50
-                            ]}
-                            totalItems={103}
-                        />
+                                            </TableHead>
+                                            <TableBody>
+                                                {rows.map((row) => (
+                                                    <TableRow key={row.id} {...getRowProps({ row })}>
+                                                        <TableSelectRow {...getSelectionProps({ row })} />
+                                                        {row.cells.map((cell) => (
+                                                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                                                        ))}
+                                                        <TableCell className="bx--table-column-menu">
+                                                            <OverflowMenu light flipped>
+                                                                <OverflowMenuItem itemText="Edit" />
+                                                            </OverflowMenu>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                )}
+                        </DataTable>
+                        <div style={{ width: '800px' }}>
+                            <Pagination
+                                backwardText="Previous page"
+                                forwardText="Next page"
+                                itemsPerPageText="Items per page:"
+                                page={1}
+                                pageNumberText="Page Number"
+                                pageSize={10}
+                                pageSizes={[
+                                    10,
+                                    20,
+                                    30,
+                                    40,
+                                    50
+                                ]}
+                                totalItems={103} />
+                        </div>
                     </div>
-                </div>
-            </div >
+                </div></>
         );
 
     }
