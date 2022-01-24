22ea8f3ae0463b7d64b116918657f4a7d942f199
Noé Samaille <noe.samaille@ibm.com>
2022-01-24 13:33:34 +0100

    Fixed Import BOM yaml with public argument

--- src/services/builder/architecture.api.ts
+++ src/services/builder/architecture.api.ts
@@ -6,7 +6,7 @@ export abstract class ArchitectureDataApi {
     abstract async addArchitecture(arch_details: Partial<ArchitectureDataModel>): Promise<ArchitectureDataModel>;
     abstract async deleteArchitecture(arch_id: string): Promise<void>;
     abstract async duplicateArchitecture(arch_id: string, data: object): Promise<ArchitectureDataModel>;
-    abstract async importBomYaml(arch_id: string, data: FormData, overwrite: boolean): Promise<object>;
+    abstract async importBomYaml(data: FormData, overwrite: boolean, publicArch: boolean): Promise<object>;
     abstract async uploadDiagrams(arch_id: string, data: FormData): Promise<any>;
     abstract async updateArchitecture(archiId: string, arch_details: any): Promise<ArchitectureDataModel>;
 }
