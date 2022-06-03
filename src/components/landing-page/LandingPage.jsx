3db2d5e14c357be86bb7638237e873d247bb58a6
Github pipeline <pipeline@github.com>
2022-06-03 17:00:52 -0500

    Menu and Landing page changes

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
 import {
-  Breadcrumb, BreadcrumbItem, Button, Tabs, Tab, Grid, Row, Column
+  Breadcrumb, BreadcrumbItem, Button, Tabs, Tab, Grid, Row, Column,UnorderedList,ListItem
 } from 'carbon-components-react';
 
 import { InfoSection, InfoCard } from './Info';
@@ -55,8 +55,12 @@ class LandingPage extends Component {
               </BreadcrumbItem>
             </Breadcrumb>
             <h1 className="landing-page__heading">
-              Architecture and Security Controls Enterprise Tool (ASCENT)
+              Software Everywhere
             </h1>
+            <h2 className="landing-page__subheading">
+              Architecture and Security Controls Enterprise Tool (ASCENT)
+            </h2>
+
           </Column>
         </Row>
         <Row className="landing-page__r2">
@@ -66,16 +70,35 @@ class LandingPage extends Component {
                 <Grid>
                   <Row className="landing-page__tab-content">
                     <Column  lg={{span: 6}}  md={{span: 4}}>
-                      <h2 className="landing-page__subheading">
-                        What is Ascent?
-                      </h2>
-                      <p className="landing-page__p">
-                        Ascent is a tool created by the IBM Ecosystem Labs to accelerate IBM partner's adoption of IBM Software on any cloud.
-                        Through automation and integration, Ascent enables enterprises to deliver compliant cloud architectures which can be
-                        clearly evidenced and communicated with Governance, Risk and Compliance teams. Click on the links below to learn more
-                        about how Ascent automates and integrates Reference Architectures, Security Controls and Cloud Services.
-                      </p>
-                      {this.state.user ? <Link to="/docs" ><Button>Learn more</Button></Link> : <Button href="/login" renderIcon={Login20}>Login</Button>}
+
+                        <h2 className="landing-page__subheading">
+                          What does this do ?
+                        </h2>
+                        <p className="landing-page__p">
+                          We have built a module which will allow Civil Infrastructure owners and operators to make sustainability-led decisions to support decarbonization
+                        </p>
+                        <p className="landing-page__p">
+                          The module provides operators and owners with three key benefits:
+                        </p>
+                        <div className="landing-page-list">
+                          <UnorderedList>
+                            <ListItem>
+                              Generates information on carbon emissions throughout design, build and operations of infrastructure, including use of resources with the lowest carbon footprint
+                            </ListItem>
+                            <ListItem>
+                              Enables benchmarking of contractors according to their green profile
+                            </ListItem>
+                            <ListItem>
+                              Automate reporting to adhere to new regulatory standards
+                            </ListItem>
+                          </UnorderedList>
+                        </div>
+                        <p className="landing-page__p">
+                          Our solution is focusing on Greenhouse gas emissions. Particularly, we focus on the indirect scope 3 emissions that stem from the company’s value chain. We do that, because ~80% of companies’ carbon emissions comes exactly from that
+                        </p>
+
+                        {this.state.user ? <Link to="/docs" ><Button>Learn more</Button></Link> : <Button href="/login" renderIcon={Login20}>Login</Button>}
+
 
                     </Column>
                     <Column lg={{span: 5}} md={{span: 4}}>
@@ -87,24 +110,31 @@ class LandingPage extends Component {
                     </Column>
                   </Row>
                   <Row className="landing-page__tab-content">
+
                     <InfoSection heading="The Principles" className="landing-page__r3">
-                      <InfoCard
-                        heading="Security Controls"
-                        body="The IBM Cloud Framework for Financial Services provides a set of pre-configured compliance controls that are adhered to by the entire ecosystem — financial institutions, cloud services, and the digital supply chain of ISVs and SaaS providers."
-                        icon={this.state.user ? <Link to={this.state.user?.roles?.includes("fs-viewer") ? "/controls" : "/nists"} ><SankeyDiagramAlt32  /></Link> : <SankeyDiagramAlt32  />}
-                      />
+
                       <InfoCard
                         heading="Reference Architectures"
-                        body="Build your solutions using our pre-defined reference architectures for IBM Cloud, AWS and Azure. Learn more about how your architecture meets your regulatory compliance and risk management obligations."
+                        body="Build your solutions using our pre-defined reference architectures for AWS, Azure or IBM Cloud. Learn more about how your architecture meets your regulatory compliance and risk management obligations."
                         icon={this.state.user ? <Link to="/boms/software" ><ModelBuilder32  /></Link> : <ModelBuilder32  />}
                       />
 
                       <InfoCard
-                        heading="Cloud Infrastructures and Services"
-                        body="Navigate our modules catalog, assemble them into your own custom reference architectures and deploy them in minutes using the infrascture as code bundles built by our solution builder!"
+                        heading="Cloud Infrastructures and Software"
+                        body="Consume the Software Everywhere  module catalog, assemble them into your own custom reference architectures and deploy them in minutes using the infrascture as code bundles built by the solution builder!"
                         icon={this.state.user ? <Link to="/services" ><Cloud32  /></Link> : <Cloud32  />}
                       />
+
+                      <InfoCard
+                          heading="Security Controls"
+                          body="The IBM Cloud Framework for Financial Services provides a set of pre-configured compliance controls that are adhered to by the entire ecosystem — financial institutions, cloud services, and the digital supply chain of ISVs and SaaS providers."
+                          icon={this.state.user ? <Link to={this.state.user?.roles?.includes("fs-viewer") ? "/controls" : "/nists"} ><SankeyDiagramAlt32  /></Link> : <SankeyDiagramAlt32  />}
+                      />
                     </InfoSection>
+
+
+
+
                   </Row>
                 </Grid>
               </Tab>
