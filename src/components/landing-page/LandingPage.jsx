106d0fb871952de39c87fd37424e101b1abff6b2
Noé Samaille <noe.samaille@ibm.com>
2022-06-17 08:44:40 +0200

    Finished fixing landing page spacing
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,6 +1,6 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import {
-  Breadcrumb, BreadcrumbItem, Button, Tabs, Tab, Grid, Row, Column,UnorderedList,ListItem
+  Breadcrumb, BreadcrumbItem, Button, Grid, Row, Column, UnorderedList, ListItem
 } from 'carbon-components-react';
 
 import { InfoSection, InfoCard } from './Info';
@@ -37,120 +37,117 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <Grid fullWidth className="landing-page">
-        <Row className="landing-page__banner">
-          <Column lg={{span: 12}}>
-            <form
-              class="genesis--MarketingBanner-marketingBannerOverview"
-              id="marketingBanner" name="marketingBanner"
-              style={{ margin: '0 -2rem', display: this.state.hideBanner ? 'none': 'flex' }}>
-              <span class="genesis--MarketingBanner-content">
-                Did you know <a href='https://www.ibm.com/training/cloud/jobroles' target="_blank" rel="noopener noreferrer">IBM Cloud Training</a> is now free?
-              </span>
-              <Close32 onClick={() => {console.log(this.state);this.setState({ hideBanner: true })}} />
-            </form>
-            <Breadcrumb noTrailingSlash aria-label="Page navigation">
-              <BreadcrumbItem>
-                <Link to="/docs" >Documentation</Link>
-              </BreadcrumbItem>
-            </Breadcrumb>
-            <h1 className="landing-page__heading">
-              Software Everywhere
-            </h1>
-            <h2 className="landing-page__subheading">
-              Create composite multi-cloud solutions rapidly with ASCENT
-            </h2>
+      <div>
+        <form
+          class="genesis--MarketingBanner-marketingBannerOverview"
+          id="marketingBanner" name="marketingBanner"
+          style={{ display: this.state.hideBanner ? 'none' : 'flex' }}>
+          <span class="genesis--MarketingBanner-content">
+            Did you know <a href='https://www.ibm.com/training/cloud/jobroles' target="_blank" rel="noopener noreferrer">IBM Cloud Training</a> is now free?
+          </span>
+          <Close32 onClick={() => { console.log(this.state); this.setState({ hideBanner: true }) }} />
+        </form>
+        <Grid fullWidth>
+          <Row className='landing-page__banner'>
+            <Column lg={{ span: 12 }}>
+              <Breadcrumb noTrailingSlash aria-label="Page navigation">
+                <BreadcrumbItem>
+                  <Link to="/docs" >Documentation</Link>
+                </BreadcrumbItem>
+              </Breadcrumb>
+              <h1 className="landing-page__heading">
+                Software Everywhere
+              </h1>
+              <h2 className="landing-page__subheading">
+                Create composite multi-cloud solutions rapidly with ASCENT
+              </h2>
 
-          </Column>
-        </Row>
-        <Row className="landing-page__r2">
-          <Column lg={{span: 12}}>
-            <Tabs aria-label="Tab navigation">
-              <Tab label="About">
-                <Grid>
-                  <Row className="landing-page__tab-content">
-                    <Column  className='landing-page__col'>
+            </Column>
+          </Row>
+          <Row className="landing-page__r2">
+            <Column lg={{ span: 12 }}>
 
-                       <h2 className="landing-page__subheading">
-                          What does Ascent do ?
-                        </h2>
-                        <p className="landing-page__p">
-                         Ascent provides a simple experience for the creation of common architecture patterns for software and cloud infrastructures to enable
-                          rapid composite solution creation
-                        </p>
-                        <div className="landing-page-list">
-                          <UnorderedList>
-                            <ListItem>
-                              Enables the fast creation of solution automation ready for production state environments
-                            </ListItem>
-                            <ListItem>
-                              Provides a set of cloud infrastructure reference architectures for AWS, Azure and IBM Cloud including Quick Start, Standard and Advanced patterns
-                            </ListItem>
-                            <ListItem>
-                              Provides a set of composite IBM Software architectures for common client and partner use-cases
-                            </ListItem>
+              <Grid fullWidth>
+                <Row className="landing-page__tab-content">
+                  <Column lg={{ span: 6 }} sm={{ span: 12 }}>
 
-                          </UnorderedList>
-                        </div>
+                    <h2 className="landing-page__subheading">
+                      What does Ascent do ?
+                    </h2>
+                    <p className="landing-page__p">
+                      Ascent provides a simple experience for the creation of common architecture patterns for software and cloud infrastructures to enable
+                      rapid composite solution creation
+                    </p>
+                    <UnorderedList nested>
+                      <ListItem>
+                        Enables the fast creation of solution automation ready for production state environments
+                      </ListItem>
+                      <ListItem>
+                        Provides a set of cloud infrastructure reference architectures for AWS, Azure and IBM Cloud including Quick Start, Standard and Advanced patterns
+                      </ListItem>
+                      <ListItem>
+                        Provides a set of composite IBM Software architectures for common client and partner use-cases
+                      </ListItem>
 
-                        <h2 className="landing-page__subheading">
-                          Why do we need Ascent ?
-                        </h2>
-                        <p className="landing-page__p">
-                          To help speed up the delivery and adoption of IBM Software on multiple cloud environment including AWS, Azure and IBM Cloud.
-                          Support the co creation of composite solutions with Clients and IBM Partners. Provide consistent automation across clouds infrastructures
-                          and software architectures.
-                        </p>
+                    </UnorderedList>
 
-                        {this.state.user ? <Link to="/docs" ><Button>Learn more</Button></Link> : <Button href="/login" renderIcon={Login20}>Login</Button>}
+                    <h2 className="landing-page__subheading">
+                      Why do we need Ascent ?
+                    </h2>
+                    <p className="landing-page__p">
+                      To help speed up the delivery and adoption of IBM Software on multiple cloud environment including AWS, Azure and IBM Cloud.
+                      Support the co creation of composite solutions with Clients and IBM Partners. Provide consistent automation across clouds infrastructures
+                      and software architectures.
+                    </p>
+
+                    {this.state.user ? <Link to="/docs" ><Button>Learn more</Button></Link> : <Button href="/login" renderIcon={Login20}>Login</Button>}
 
 
-                    </Column>
-                    <Column  className='landing-page__col'>
-                      <h2 className="landing-page__subheading">
-                        Architecture and Security Controls Enterprise Tool (ASCENT)
-                      </h2>
+                  </Column>
+                  <Column lg={{ span: 6 }} sm={{ span: 12 }}>
+                    <h2 className="landing-page__subheading">
+                      Architecture and Security Controls Enterprise Tool (ASCENT)
+                    </h2>
 
-                      <img
-                        className="landing-page__illo"
-                        src={`${process.env.PUBLIC_URL}/ascent.png`}
-                        alt="Tool illustration"
-                      />
-                    </Column>
-                  </Row>
-                  <Row className="landing-page__tab-content">
+                    <img
+                      className="landing-page__illo"
+                      src={`${process.env.PUBLIC_URL}/ascent.png`}
+                      alt="Tool illustration"
+                    />
+                  </Column>
+                </Row>
+                <Row className="landing-page__tab-content">
 
-                    <InfoSection heading="The Principles" className="landing-page__r3">
+                  <InfoSection heading="The Principles" className="landing-page__r3">
 
-                      <InfoCard
-                        heading="Reference Architectures"
-                        body="Build your solutions using our pre-defined reference architectures for AWS, Azure or IBM Cloud. Learn more about how your architecture meets your regulatory compliance and risk management obligations."
-                        icon={this.state.user ? <Link to="/boms/software" ><ModelBuilder32  /></Link> : <ModelBuilder32  />}
-                      />
+                    <InfoCard
+                      heading="Reference Architectures"
+                      body="Build your solutions using our pre-defined reference architectures for AWS, Azure or IBM Cloud. Learn more about how your architecture meets your regulatory compliance and risk management obligations."
+                      icon={this.state.user ? <Link to="/boms/software" ><ModelBuilder32 /></Link> : <ModelBuilder32 />}
+                    />
 
-                      <InfoCard
-                        heading="Cloud Infrastructures and Software"
-                        body="Consume the Software Everywhere  module catalog, assemble them into your own custom reference architectures and deploy them in minutes using the infrascture as code bundles built by the solution builder!"
-                        icon={this.state.user ? <Link to="/services" ><Cloud32  /></Link> : <Cloud32  />}
-                      />
+                    <InfoCard
+                      heading="Cloud Infrastructures and Software"
+                      body="Consume the Software Everywhere  module catalog, assemble them into your own custom reference architectures and deploy them in minutes using the infrascture as code bundles built by the solution builder!"
+                      icon={this.state.user ? <Link to="/services" ><Cloud32 /></Link> : <Cloud32 />}
+                    />
 
-                      <InfoCard
-                          heading="Security Controls"
-                          body="The IBM Cloud Framework for Financial Services provides a set of pre-configured compliance controls that are adhered to by the entire ecosystem — financial institutions, cloud services, and the digital supply chain of ISVs and SaaS providers."
-                          icon={this.state.user ? <Link to={this.state.user?.roles?.includes("fs-viewer") ? "/controls" : "/nists"} ><SankeyDiagramAlt32  /></Link> : <SankeyDiagramAlt32  />}
-                      />
-                    </InfoSection>
+                    <InfoCard
+                      heading="Security Controls"
+                      body="The IBM Cloud Framework for Financial Services provides a set of pre-configured compliance controls that are adhered to by the entire ecosystem — financial institutions, cloud services, and the digital supply chain of ISVs and SaaS providers."
+                      icon={this.state.user ? <Link to={this.state.user?.roles?.includes("fs-viewer") ? "/controls" : "/nists"} ><SankeyDiagramAlt32 /></Link> : <SankeyDiagramAlt32 />}
+                    />
+                  </InfoSection>
 
 
 
 
-                  </Row>
-                </Grid>
-              </Tab>
-            </Tabs>
-          </Column>
-        </Row>
-      </Grid>
+                </Row>
+              </Grid>
+            </Column>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 };
