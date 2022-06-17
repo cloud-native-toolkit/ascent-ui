bf2ec7213fac14710e04a0f877630fe6f8b746ec
Noé Samaille <noe.samaille@ibm.com>
2022-06-17 09:45:04 +0200

    Fixed test bug
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders header', () => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }))
+  });
   render(<App />);
   const linkElement = screen.getByText(/Architecture and Security Controls Enterprise Tool/i);
   expect(linkElement).toBeInTheDocument();
