3d4a74194fac9d960a408bbf1dd3ee5573e98feb
Noe-Samaille <noe.samaille@ibm.com>
2022-11-10 15:18:07 +0100

    Updates package to include server
    Signed-off-by: Noe-Samaille <noe.samaille@ibm.com>

--- scripts/packageApp.sh
+++ scripts/packageApp.sh
@@ -5,7 +5,7 @@ set -e
 # The following files and folders are copied by default in the CIO CI/CD Pipeline:
 # package.json, package-lock.json, node_modules
 # These files are the other resources required for the app to run
-FOLDERS_TO_COPY=("config" "dist" "public")
+FOLDERS_TO_COPY=("config" "dist" "public" "server")
 
 # Prune the node_dependencies
 npm prune --omit=dev
